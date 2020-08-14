182ea33b99a26aa237e9c57cd4042918d7e8eb97
Aasterisk <54024986+Aasterisk@users.noreply.github.com>
2020-08-14 20:34:54 +0800

    AssetsBundle

---
+++ AssetBundleProject/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles  {
+    //构建assetsbundle包
+    [MenuItem("Assets/Build AssetBundles")]//使用了特性，将其放到菜单栏
+    static void BuildAllAssetBundles()
+    {
+        string dir = "AssetBundles";
+        if( Directory.Exists(dir)==false)
+        {
+            Directory.CreateDirectory(dir);
+        }
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows64);
+    }
+	
+}
